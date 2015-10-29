1b8505a30a9693bf7a14b2c0754a4d8c343710aa
Karan Jeet <karanjeet96@gmail.com>
2015-10-30 00:32:41 +0530

    mongo lab conn2

--- main/php/test.php
+++ main/php/test.php
@@ -1,23 +1,11 @@
 <?php
-   echo"test!!";
-
-   $dbname = 'artistarc_alpha';
-  
-try{
+ini_set("display_errors", "On");
  
-$m = new MongoClient( "mongodb://karanjeet96:wazxdws123@ds052408.mongolab.com:52408/artistarc_alpha" ); // connect to a remote host at a given port
-   echo "Connection to database successfully<br>";
-$db = $m->$dbname;
-   echo "Database ". $dbname ." selected<br>";
-
-$collection = $db->artist_reg;
-$cursor = $collection->find();
-    echo "success!<br>";
-
- foreach ($cursor as $document) {
-      echo $document["name"] . "\n";
-   }
-} catch (Exception $e){
+try{
+    $db = new Mongo("mongodb://karanjeet96:wazxdws123@ds052408.mongolab.com:52408/artistarc_alpha");
+    $registrations = $db->selectCollection('artistarc_alpha', 'registrations');
+        echo "sucess!<br />";
+}catch (Exception $e){
     echo 'Caught exception: ',  $e->getMessage(), "<br />";
 }
 ?>
