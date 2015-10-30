039be3a28b3001590f1b094f5bb394b9db31c521
Karan Jeet <karanjeet96@gmail.com>
2015-10-30 07:40:56 +0530

    mongo driver php

--- test.php
+++ test.php
@@ -1,15 +1,30 @@
 <?php
-    
-include "main/php/connect.php";
+ini_set("display_errors", "On");
+ 
+try{
+    $mongo = new Mongo("mongodb://karanjeet96:wazxdws123@ds052408.mongolab.com:52408/artistarc_alpha");
+    //$registrations = $db->selectCollection('artistarc_alpha', 'registrations');
+        echo "sucess!<br />";
+        $db = $mongo->artistarc_alpha;
+        $collection = $db->artist_reg;
+        $cursor = $collection->find();
+ 
+ echo "success!<br>";
 
-echo "test";
+}catch (Exception $e){
+    echo 'Caught exception: ',  $e->getMessage(), "<br />";
+}
+
+ foreach ($cursor as $document) {
+      echo $document["name"] . "\n";
+   }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>testyo</title>
     </head>
     <body>
         
